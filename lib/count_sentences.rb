56443b9d25de1e7ee9cc7de22134545a91b1fa4b
kamausuzan197 <kamaususan197@gmail.com>
2022-08-22 10:36:56 +0300

    complete

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,24 @@
 require 'pry'
 
 class String
-
+  
   def sentence?
-    
+   #  In Ruby, there's a convention to name methods that return true or false with a question mark (?) at the end, like the Array#include? or Array#any? methods.
+   #You can use the #end_with? method on a string to determine what its final character is
+   self.end_with?('.')
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    sent_end = 
+    arr = self.split(/[.?!] /)
+    arr.length
   end
 end
